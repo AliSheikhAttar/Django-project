fc30681913f380243e8b929ed364788e2b27e497
AliSheikh <ali.sheikhattar@yahoo.com>
2023-06-21 17:12:10 +0330

    edit

--- mysite5/mysite5/settings.py
+++ mysite5/mysite5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cnts!s94-&g_5$5o&2c81*lqzewgpvz(lza)h^hggrifk85du@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
